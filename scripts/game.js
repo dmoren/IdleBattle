663ffbd9d97770aa033ff9192c2a35626815754c
dmoren <diego.moreno90@gmail.com>
2015-07-30 07:24:28 -0300

    changed navbar and added arrows for minions, and optimized js code

--- scripts/game.js
+++ scripts/game.js
@@ -8,42 +8,22 @@ var charLevel = 1;
 var expRequired = 25*charLevel*(1+charLevel);
 var warriors = 0;
 
-function attack() {
+function attack(dmg) {
 	minionHP = minionHP - dmg;
 	updateHTML("minionHP", minionHP);
-	//document.getElementById('minionHP').innerHTML = minionHP;
-	if (minionHP <= 0) {
-		spawnMonster();
-		getReward();
-		checkExp();
-	};
+	checkMinion();
 };
 
-function autoAttack(number) {
-	minionHP = minionHP - warriors;
-	updateHTML("minionHP", minionHP);
-	//document.getElementById('minionHP').innerHTML = minionHP;
-	if (minionHP <= 0) {
-		spawnMonster();
-		getReward();
-		checkExp();
-	};
-};
-
-function spawnMonster() {
-	var maxhp = minionLevel * 5;
-	expReward = minionLevel;
-	minionHP = maxhp;
-	updateHTML("minionHP", minionHP);
-};
-
-function getReward() {
-	var calcMoneyDrop = Math.floor(1.1 * minionLevel); //Averiguamos el gold que dropea
-	money = money + calcMoneyDrop; //agrega el gold
-	var expReward = Math.floor(1.1 * minionLevel);
-	charExp = charExp + expReward;
-	updateHTML("money", money); //actualiza el valor al usuario
-	updateHTML("charExp", charExp); //actualiza el valor al usuario
+function buyWarrior() {
+    var warriorCost = Math.floor(10 * Math.pow(1.1, warriors)); //works out the cost of this cursor
+    if (money >= warriorCost) { //checks that the player can afford the cursor
+        warriors++; //increases number of cursors
+        money = money - warriorCost; //removes the cookies spent
+        updateHTML("warriorID", warriors);
+        updateHTML("money", money);
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1, warriors)); //works out the cost of the next cursor
+    updateHTML("warriorCostID", nextCost) ; //updates the cursor cost for the user
 };
 
 function upgradeMinion() {
@@ -59,9 +39,25 @@ function upgradeMinion() {
     updateHTML("upgradeCost", nextUpgradeCost); //updates the cursor cost for the user
 };
 
-function checkExp() {
-	//var expRequired = Math.pow(charLevel, charLevel) + lastExpRequirement;
-	//var expRequired = 25*charLevel*(1+charLevel)
+function checkMinion() {
+	if (minionHP <= 0) {
+		getReward();
+		spawnMonster();
+	};
+}
+
+function spawnMonster() {
+	var maxhp = minionLevel * 5;
+	expReward = minionLevel;
+	minionHP = maxhp;
+	updateHTML("minionHP", minionHP);
+};
+
+function getReward() {
+	var calcMoneyDrop = Math.floor(1.1 * minionLevel); //Averiguamos el gold que dropea
+	money = money + calcMoneyDrop; //agrega el gold
+	var expReward = Math.floor(1.1 * minionLevel);
+	charExp = charExp + expReward;
 	if (charExp >= expRequired) {
 		dmg = Math.floor(charLevel * 1.5);
 		charLevel++;
@@ -71,27 +67,17 @@ function checkExp() {
 		updateHTML("dmg", dmg);
 		updateHTML("requiredExp", expRequired);
 	};
+	updateHTML("money", money); //actualiza el valor al usuario
+	updateHTML("charExp", charExp); //actualiza el valor al usuario
+
 };
 
 function updateHTML(elementID, value) {
 	document.getElementById(elementID).innerHTML = value;
 }
 
-function buyWarrior() {
-    var warriorCost = Math.floor(10 * Math.pow(1.1, warriors)); //works out the cost of this cursor
-    if (money >= warriorCost) { //checks that the player can afford the cursor
-        warriors++; //increases number of cursors
-        money = money - warriorCost; //removes the cookies spent
-        updateHTML("warriorID", warriors);
-        updateHTML("money", money);
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1, warriors)); //works out the cost of the next cursor
-    updateHTML("warriorCostID", nextCost) ; //updates the cursor cost for the user
-};
-
-
 window.setInterval(function() {
 
-    autoAttack(warriors);
+    attack(warriors);
 
 }, 1000);
